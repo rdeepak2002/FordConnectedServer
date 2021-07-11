5cd462965052612ed9305fe5485d88b1b3c4b75d
rdeepak2002 <rdeepak2002@gmail.com>
2021-07-11 01:13:33 -0700

    created friend request system

--- src/main/java/com/deepdev/fordconnected/server/resolver/Query.java
+++ src/main/java/com/deepdev/fordconnected/server/resolver/Query.java
@@ -3,7 +3,6 @@ package com.deepdev.fordconnected.server.resolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
